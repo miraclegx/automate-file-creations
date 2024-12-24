45ff89938eeca4e49b904de50e65c84f6ff4c411
miracle iyanu <miracleojonugwamaji@gmail.com>
2024-12-24 22:17:14 +0100

    came back after a while, it now shows a pop up after the file is create

--- make lua file/automate.py
+++ make lua file/automate.py
@@ -2,23 +2,14 @@
 import os
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 def save_to():
 	file_path = tk.filedialog.asksaveasfilename(defaultextension='.lua')
 	if file_path:
 		with open(file_path,'w') as file:
-			file.write('''function love.load()
-
-end
-	
-function love.update(dt)
-	
-end
-	
-function love.draw()
-	
-end''')
-
+			file.write('''function love.load()\nend\nfunction love.update(dt)\nend\nfunction love.draw()\nend''')
+	pop_up  = messagebox.showinfo('very boring guy','file created')
 
 root = tk.Tk()
 root.geometry('200x100')
@@ -30,7 +21,7 @@ label = tk.Label(root,text='Enter A Name For The File').grid(row = 1,column = 0,
 '''f_name = tk.Entry(root,width = 5)
 f_name.grid(column = 5,row = 1,padx = 10)'''
 
-#butoon to selct save directory
+#buton to selct save directory
 save_btn = tk.Button(root,text = 'clic me',command = save_to)
 save_btn.grid(column = 0,row = 2,pady = 0)
 
